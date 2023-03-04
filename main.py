2429af2a4ecfaa5bd8bcacdae836ef076ad81fc7
uniq47 <unique.sapkota01@student.csulb.edu>
2023-03-03 20:08:13 -0800

    added basemodel, Extracted data

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
+# body is used to extract data from the body of the request and assign it to a variable
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+# FastAPI is a class and we are creating an instance of it and storing it in a variable called app
+# Post method will  extends BaseModel class
+
+
+class Post(BaseModel):
+    firstname: str
+    lastname: str
 
 
 @app.get("/")
-async def root():
+async def root():  # async that takes certain amount of time to execute and we dont want to wait for it to execute
     return {"message": "welcome to the world of computer science"}
-
-# A BUNCH OF SOCIAL MEDIA POSTS FRO OUR APPLICAOTIN
 
 
 @app.get("/posts")
 def get_posts():
+    # logic to retrive the posts from the database
     return {"data": "This is your posts"}
+
+# in create post we want to tell front what data we are expecting, we want two string
 
 
 @app.post("/createposts")
-# payload is my retrive the body data, assign a var to it ours is payload
-# type dict , ... helps us to get the body data from the request and assign it to payload
-# extract all the fields fro mthe body and covert it tothe dictornary  and store it inside a variable named pauyload
-def create_posts(payload: dict = Body(...)): #extracting data from the body 
-    # print(payload)
-    return {"new_post": f"email {payload['email']} firstName: {payload['firstName']}"}
+# def create_posts(payload: dict = Body(...)):  # extracting data from the body
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())  # convert the object to dictionary
+    return {"data": post}
+
+
+# inside payload we have email and firstName.
+# {
+#     "firstname": "aayam",
+#     "lastname": "sapkotaaaaaaa"
+# }
