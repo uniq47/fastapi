b6272e5f54a3fdf10fbc1f1999f417fac67ab11f
uniq47 <unique.sapkota01@student.csulb.edu>
2023-03-08 08:53:38 -0800

    added delete

--- main.py
+++ main.py
@@ -25,6 +25,11 @@ def find_item(id):
     for p in new_post:
         if p["id"] == id:
             return p 
+        
+def find_index_post(id):
+    for i, p in enumerate(new_post):
+        if p["id"]==id:
+            return i 
 
 @app.get("/")
 async def root():  # async that takes certain amount of time to execute and we dont want to wait for it to execute
@@ -51,8 +56,17 @@ def create_posts(post: Post):
 def get_post(id: int, response: Response):
     post = find_item(id)
     if not post:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"post not found with id {id}")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"error": "post not found"}
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                            detail=f"post not found with id {id}")
     return {"data": post}
+
+@app.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(id :int):
+    index = find_index_post(id)
+    if index ==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post not found with id {id}")
+    new_post.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
 
